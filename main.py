ae922cc6e0b55a33c5556965f8a40706ed9a54e3
Tjorriemorrie <jacoj82@gmail.com>
2014-06-16 09:54:10 +0200

    config broken

---
+++ main.py
@@ -0,0 +1,17 @@
+import webapp2
+import jinja2
+import os
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.out.write(template.render())
+
+
+app = webapp2.WSGIApplication([
+  ('/', MainHandler),
+], debug=True)
